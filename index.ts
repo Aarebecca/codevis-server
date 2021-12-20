f68b95c22dd33674b91dfbce02f7abbf95e7a15e
Aaron <943720372@qq.com>
2021-12-20 19:26:18 +0800

    refactor(express): refactor api logic

--- index.ts
+++ index.ts
@@ -1,23 +1,50 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { config } from "./express.config";
-import { varFlowPipeline } from "./src";
+import { varFlowPipeline, initLogger } from "./src";
+import cors from "cors";
+
+const logger = initLogger();
+logger.info("start service");
+
+const CONSTANTS = {
+  success: "ok",
+  error: "fail",
+};
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
 app.post("/var-flow", (req, res) => {
-  const { code } = req.body;
+  const { code, mixer = "average", nodeColor = "{}" } = req.body;
   if (!code) {
     res.status(400).send("code is required");
     return;
   }
-  res.send(JSON.stringify(varFlowPipeline(code)));
+  try {
+    res.send({
+      status: CONSTANTS.success,
+      data: varFlowPipeline(code, mixer, nodeColor),
+    });
+  } catch (e) {
+    // log
+    logger.error(`var-flow error: ${e}`);
+    res.send({
+      status: CONSTANTS.error,
+      message: "解析失败",
+    });
+  }
 });
 
 app.listen(config.port, () => {
